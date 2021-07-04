ae8a583da00635bdddada6c8d5d163cecc087a92
Nitin Verma <canvas.nv@gmail.com>
2021-07-04 14:40:08 +0530

    compressed image and added method to hide/show keyboard
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

--- app/src/main/java/com/andryoga/safebox/ui/common/Utils.kt
+++ app/src/main/java/com/andryoga/safebox/ui/common/Utils.kt
@@ -1,6 +1,8 @@
 package com.andryoga.safebox.ui.common
 
+import android.content.Context
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 
 object Utils {
@@ -22,5 +24,14 @@ object Utils {
 
     fun setTextViewLeftDrawable(view: TextView, drawableId: Int) {
         view.setCompoundDrawablesWithIntrinsicBounds(drawableId, 0, 0, 0)
+    }
+
+    fun setKeyBoardVisibility(context: Context, show: Boolean) {
+        val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        if (show) {
+            imm.toggleSoftInput(InputMethodManager.SHOW_IMPLICIT, 0)
+        } else {
+            imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0)
+        }
     }
 }
