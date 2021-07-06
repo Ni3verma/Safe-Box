c2a021133ee792168f6af3024182182767f3b702
Bhawna Adhikari <bhawna.ad05@gmail.com>
2021-07-06 12:25:43 +0530

    Calling repo method in viewmodel

--- app/src/main/java/com/andryoga/safebox/ui/view/login/LoginViewModel.kt
+++ app/src/main/java/com/andryoga/safebox/ui/view/login/LoginViewModel.kt
@@ -21,7 +21,13 @@ class LoginViewModel @Inject constructor(
         encryptedPreferenceProvider.getBooleanPref(IS_SIGN_UP_REQUIRED, true)
 
     val pswrd = MutableLiveData("Qwerty@@135")
-    val hint = MutableLiveData("Here's a hint")
+    val hint = MutableLiveData("")
+
+    init {
+        viewModelScope.launch {
+            hint.postValue(userDetailsRepository.getHint())
+        }
+    }
 
     private val _isWrongPswrdEntered = MutableLiveData<Boolean>()
     val isWrongPswrdEntered: LiveData<Boolean> = _isWrongPswrdEntered
