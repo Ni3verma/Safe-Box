8443d7b67a63bef36e9d91d4e432a3a89a3a7f71
Nitin Verma <canvas.nv@gmail.com>
2021-07-09 12:27:48 +0530

    added support to encrypt empty data as null
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

--- app/src/main/java/com/andryoga/safebox/common/Utils.kt
+++ app/src/main/java/com/andryoga/safebox/common/Utils.kt
@@ -1,10 +1,19 @@
 package com.andryoga.safebox.common
 
+import com.andryoga.safebox.security.interfaces.SymmetricKeyUtils
 import com.andryoga.safebox.ui.common.Resource
 import timber.log.Timber
 
 object Utils {
     fun logResourceInfo(tag: String, resource: Resource<Any>) {
         Timber.i("$tag --> status = ${resource.status}, data = ${resource.data}, message = ${resource.message}")
+    }
+
+    fun String?.encryptNullableString(symmetricKeyUtils: SymmetricKeyUtils): String? {
+        return if (this.isNullOrBlank()) null else symmetricKeyUtils.encrypt(this)
+    }
+
+    fun String?.decryptNullableString(symmetricKeyUtils: SymmetricKeyUtils): String? {
+        return if (this.isNullOrBlank()) null else symmetricKeyUtils.decrypt(this)
     }
 }
