b2c89138c7dd965e390629d5ade36099d60c1314
Nitin Verma <canvas.nv@gmail.com>
2021-07-31 14:43:13 +0530

    Added validation and handling save btn click
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

--- app/src/main/java/com/andryoga/safebox/ui/view/home/addNewData/bankCard/AddNewBankCardDialogFragment.kt
+++ app/src/main/java/com/andryoga/safebox/ui/view/home/addNewData/bankCard/AddNewBankCardDialogFragment.kt
@@ -4,18 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import com.andryoga.safebox.R
+import com.andryoga.safebox.common.Utils
 import com.andryoga.safebox.databinding.DialogAddNewBankCardDataBinding
+import com.andryoga.safebox.ui.common.CommonSnackbar
+import com.andryoga.safebox.ui.common.RequiredFieldValidator
+import com.andryoga.safebox.ui.common.Resource
+import com.andryoga.safebox.ui.common.Status
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-import timber.log.Timber
 
 class AddNewBankCardDialogFragment : BottomSheetDialogFragment() {
 
     private val viewModel: AddNewBankCardViewModel by viewModels()
     private lateinit var binding: DialogAddNewBankCardDataBinding
+    private val tagLocal = "add new bank card dialog fragment"
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,10 +40,62 @@ class AddNewBankCardDialogFragment : BottomSheetDialogFragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.expiryDateText.doAfterTextChanged { currentText ->
-            if (currentText?.toString()?.length == 2) {
-                // gotta handle this
-                Timber.i("$currentText")
+        /* whenever two characters(basically month) are entered in expiry date
+        * then append "/" in the end.
+        * Here start = 1 means I am entering 2nd character and 
+        * before = 0 means I am not deleting a character and moving backwards
+        * */
+        binding.expiryDateText.doOnTextChanged { _, start, before, _ ->
+            if (start == 1 && before == 0) {
+                binding.expiryDateText.append("/")
+            }
+        }
+
+        binding.saveBtn.setOnClickListener {
+            viewModel.onSaveClick().observe(viewLifecycleOwner) {
+                handleSaveButtonClick(it)
+            }
+        }
+
+        val requiredFieldValidator = RequiredFieldValidator(
+            listOf(
+                binding.title,
+                binding.number,
+                binding.cvv,
+                binding.expiryDate
+            ),
+            binding.saveBtn,
+            tagLocal
+        )
+        requiredFieldValidator.validate()
+    }
+
+    private fun handleSaveButtonClick(resource: Resource<Boolean>) {
+        // NEED_HELP: 6/19/2021 Not able to figure out
+        // why snackbar is not showing up if I use requireView() in view param
+        Utils.logResourceInfo(tagLocal, resource)
+        when (resource.status) {
+            Status.LOADING -> com.andryoga.safebox.ui.common.Utils.switchVisibility(
+                binding.saveBtn,
+                binding.loading
+            )
+            Status.SUCCESS -> {
+                CommonSnackbar.showSuccessSnackbar(
+                    activity!!.findViewById(R.id.drawer_layout),
+                    getString(R.string.snackbar_common_data_saved)
+                )
+                dismiss()
+            }
+            Status.ERROR -> {
+                com.andryoga.safebox.ui.common.Utils.switchVisibility(
+                    binding.saveBtn,
+                    binding.loading
+                )
+                CommonSnackbar.showErrorSnackbar(
+                    activity!!.findViewById(R.id.drawer_layout),
+                    getString(R.string.snackbar_common_error_saving_data)
+                )
+                dismiss()
             }
         }
     }
--- app/src/main/java/com/andryoga/safebox/ui/view/home/addNewData/bankCard/AddNewBankCardViewModel.kt
+++ app/src/main/java/com/andryoga/safebox/ui/view/home/addNewData/bankCard/AddNewBankCardViewModel.kt
@@ -1,7 +1,28 @@
 package com.andryoga.safebox.ui.view.home.addNewData.bankCard
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import com.andryoga.safebox.ui.common.Resource
+import timber.log.Timber
 
 class AddNewBankCardViewModel : ViewModel() {
     val addNewBankCardScreenData = AddNewBankCardScreenData()
+
+    fun onSaveClick() = liveData(viewModelScope.coroutineContext) {
+        Timber.i("save clicked, adding bank card data in db")
+        emit(Resource.loading(true))
+        try {
+//            loginDataRepository.insertLoginData(addNewLoginScreenData)
+            emit(Resource.success(true))
+        } catch (ex: Exception) {
+            emit(
+                Resource.error(
+                    data = false,
+                    message = ex.message
+                )
+            )
+            Timber.e(ex)
+        }
+    }
 }
