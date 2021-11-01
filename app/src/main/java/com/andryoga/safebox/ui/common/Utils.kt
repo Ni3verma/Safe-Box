2246ede1ecf79c55662fdf81a126e9abbadf04c4
Nitin Verma <canvas.nv@gmail.com>
2021-11-01 14:08:03 +0530

    Added share data action (#102)
    * copying data on action click and field click
    
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>
    
    * sharing data only for card and account
    
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>
    
    * small ui tweaks
    
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

--- app/src/main/java/com/andryoga/safebox/ui/common/Utils.kt
+++ app/src/main/java/com/andryoga/safebox/ui/common/Utils.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.TextView
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.databinding.ObservableField
 
@@ -25,22 +24,10 @@ object Utils {
         }
     }
 
-    fun setTextViewLeftDrawable(view: TextView, drawableId: Int) {
-        view.setCompoundDrawablesWithIntrinsicBounds(drawableId, 0, 0, 0)
-    }
-
     fun hideSoftKeyboard(activity: Activity) {
         val imm = activity.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         if (activity.currentFocus != null) {
             imm.hideSoftInputFromWindow(activity.currentFocus!!.windowToken, 0)
-        }
-    }
-
-    fun switchText(view: TextView, originalText: String, changeToText: String) {
-        if (view.text == originalText) {
-            view.text = changeToText
-        } else {
-            view.text = originalText
         }
     }
 
