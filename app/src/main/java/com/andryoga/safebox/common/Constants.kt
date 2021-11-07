8933a2d118501917a4e84fd0fdc86ffb0cbaac66
Nitin Verma <canvas.nv@gmail.com>
2021-11-07 22:47:30 +0530

    exporting worker code done
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

--- app/src/main/java/com/andryoga/safebox/common/Constants.kt
+++ app/src/main/java/com/andryoga/safebox/common/Constants.kt
@@ -11,8 +11,21 @@ object Constants {
     // crashlytics
     const val CRASHLYTICS_KEY_UID = "uid"
 
+    // export and import
+    const val VERSION_KEY = "VERSION"
+    const val SALT_KEY = "SALT"
+    const val IV_KEY = "IV"
+    const val CREATION_DATE_KEY = "CREATION_DATE"
+    const val LOGIN_DATA_KEY = "LOGIN_DATA"
+    const val BANK_ACCOUNT_DATA_KEY = "BANK_ACCOUNT_DATA"
+    const val BANK_CARD_DATA_KEY = "BANK_CARD_DATA"
+    const val SECURE_NOTE_DATA_KEY = "SECURE_NOTE_DATA"
+
+    const val EXPORT_IMPORT_VERSION = 1
+
     //        Other
     const val time1Sec = 1000L
     const val APP_GITHUB_URL = "https://github.com/Ni3verma/Safe-Box"
-    const val APP_PLAYSTORE_LINK = "https://play.google.com/store/apps/details?id=com.andryoga.safebox"
+    const val APP_PLAYSTORE_LINK =
+        "https://play.google.com/store/apps/details?id=com.andryoga.safebox"
 }
--- app/src/main/java/com/andryoga/safebox/worker/ExportDataWorker.kt
+++ app/src/main/java/com/andryoga/safebox/worker/ExportDataWorker.kt
@@ -4,7 +4,17 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
+import com.andryoga.safebox.common.Constants
+import com.andryoga.safebox.common.Constants.CREATION_DATE_KEY
+import com.andryoga.safebox.common.Constants.EXPORT_IMPORT_VERSION
+import com.andryoga.safebox.common.Constants.IV_KEY
+import com.andryoga.safebox.common.Constants.SALT_KEY
+import com.andryoga.safebox.common.Constants.VERSION_KEY
 import com.andryoga.safebox.common.Constants.time1Sec
+import com.andryoga.safebox.data.db.docs.export.ExportBankAccountData
+import com.andryoga.safebox.data.db.docs.export.ExportBankCardData
+import com.andryoga.safebox.data.db.docs.export.ExportLoginData
+import com.andryoga.safebox.data.db.docs.export.ExportSecureNoteData
 import com.andryoga.safebox.data.db.secureDao.BankAccountDataDaoSecure
 import com.andryoga.safebox.data.db.secureDao.BankCardDataDaoSecure
 import com.andryoga.safebox.data.db.secureDao.LoginDataDaoSecure
@@ -14,7 +24,11 @@ import com.andryoga.safebox.security.interfaces.SymmetricKeyUtils
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.json.Json
 import timber.log.Timber
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
 import kotlin.properties.Delegates
 
 @HiltWorker
@@ -32,6 +46,9 @@ class ExportDataWorker
 ) : CoroutineWorker(context, params) {
 
     private var startTime by Delegates.notNull<Long>()
+
+    private lateinit var salt: ByteArray
+    private lateinit var iv: ByteArray
 
     override suspend fun doWork(): Result {
 
@@ -53,15 +70,112 @@ class ExportDataWorker
             shouldExport(loginData, bankAccountData, bankCardData, secureNoteData)
         ) {
             Timber.i("data is present for export")
-            val salt = passwordBasedEncryption.getRandomSalt()
-            val iv = passwordBasedEncryption.getRandomIV()
+            salt = passwordBasedEncryption.getRandomSalt()
+            iv = passwordBasedEncryption.getRandomIV()
+            val exportMap = mutableMapOf<String, Any>(
+                SALT_KEY to salt,
+                IV_KEY to iv,
+                VERSION_KEY to EXPORT_IMPORT_VERSION
+            )
             recordTime("got salt and iv")
-            Timber.i("$salt\n$iv")
+
+            val loginDataByteArray: ByteArray? = exportLoginData(loginData, inputPassword)
+            recordTime("got login data byte array")
+            if (loginDataByteArray != null) {
+                exportMap[Constants.LOGIN_DATA_KEY] = loginDataByteArray
+            }
+
+            val bankAccountDataByteArray: ByteArray? =
+                exportBankAccountData(bankAccountData, inputPassword)
+            recordTime("got bank account data byte array")
+            if (bankAccountDataByteArray != null) {
+                exportMap[Constants.BANK_ACCOUNT_DATA_KEY] = bankAccountDataByteArray
+            }
+
+            val bankCardDataByteArray: ByteArray? = exportBankCardData(bankCardData, inputPassword)
+            recordTime("got bank card data byte array")
+            if (bankCardDataByteArray != null) {
+                exportMap[Constants.BANK_CARD_DATA_KEY] = bankCardDataByteArray
+            }
+
+            val secureNoteDataByteArray: ByteArray? =
+                exportSecureNoteData(secureNoteData, inputPassword)
+            recordTime("got secure note data byte array")
+            if (secureNoteDataByteArray != null) {
+                exportMap[Constants.SECURE_NOTE_DATA_KEY] = secureNoteDataByteArray
+            }
+
+            exportMap[CREATION_DATE_KEY] = System.currentTimeMillis()
+
+            ObjectOutputStream(FileOutputStream("nitinTestEnc")).use {
+                it.writeObject(exportMap)
+            }
+            recordTime("exported to file")
         } else {
             Timber.i("nothing to export")
         }
 
         return Result.Success()
+    }
+
+    private fun exportLoginData(
+        data: List<ExportLoginData>,
+        inputPassword: String
+    ): ByteArray? {
+        if (data.isNotEmpty()) {
+            val json = Json.encodeToString(ListSerializer(ExportLoginData.serializer()), data)
+            return passwordBasedEncryption.encryptDecrypt(
+                symmetricKeyUtils.decrypt(inputPassword).toCharArray(),
+                json.toByteArray(),
+                salt, iv, true
+            )
+        }
+        return null
+    }
+
+    private fun exportBankAccountData(
+        data: List<ExportBankAccountData>,
+        inputPassword: String
+    ): ByteArray? {
+        if (data.isNotEmpty()) {
+            val json = Json.encodeToString(ListSerializer(ExportBankAccountData.serializer()), data)
+            return passwordBasedEncryption.encryptDecrypt(
+                symmetricKeyUtils.decrypt(inputPassword).toCharArray(),
+                json.toByteArray(),
+                salt, iv, true
+            )
+        }
+        return null
+    }
+
+    private fun exportBankCardData(
+        data: List<ExportBankCardData>,
+        inputPassword: String
+    ): ByteArray? {
+        if (data.isNotEmpty()) {
+            val json = Json.encodeToString(ListSerializer(ExportBankCardData.serializer()), data)
+            return passwordBasedEncryption.encryptDecrypt(
+                symmetricKeyUtils.decrypt(inputPassword).toCharArray(),
+                json.toByteArray(),
+                salt, iv, true
+            )
+        }
+        return null
+    }
+
+    private fun exportSecureNoteData(
+        data: List<ExportSecureNoteData>,
+        inputPassword: String
+    ): ByteArray? {
+        if (data.isNotEmpty()) {
+            val json = Json.encodeToString(ListSerializer(ExportSecureNoteData.serializer()), data)
+            return passwordBasedEncryption.encryptDecrypt(
+                symmetricKeyUtils.decrypt(inputPassword).toCharArray(),
+                json.toByteArray(),
+                salt, iv, true
+            )
+        }
+        return null
     }
 
     private fun recordTime(message: String) {
