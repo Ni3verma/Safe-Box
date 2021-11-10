cec22243250bccffd0ff8ea4214df68dc9c9994a
Nitin Verma <canvas.nv@gmail.com>
2021-11-10 16:14:21 +0530

    started with ui
    Signed-off-by: Nitin Verma <canvas.nv@gmail.com>

---
+++ app/src/main/java/com/andryoga/safebox/ui/view/home/child/backupAndRestore/BackupAndRestoreFragment.kt
@@ -0,0 +1,29 @@
+package com.andryoga.safebox.ui.view.home.child.backupAndRestore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BackupAndRestoreFragment : Fragment() {
+    private val viewModel: BackupAndRestoreViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                Text(text = "hey there")
+            }
+            viewModel.deleteItLater
+        }
+    }
+}
