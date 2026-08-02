8737a522de6182df502430af7e54712cd0c819f4
Nitin Verma <canvas.nv@gmail.com>
2026-08-02 15:40:15 +0530

    allow backup with any pswrd

--- app/src/test/java/com/andryoga/safebox/worker/BackupDataWorkerTest.kt
+++ app/src/test/java/com/andryoga/safebox/worker/BackupDataWorkerTest.kt
@@ -182,6 +182,7 @@ class BackupDataWorkerTest {
         val result = worker.doWork()
 
         assertThat(result).isEqualTo(Result.success())
+        assertThat(analyticsHelper.hasLogged(AnalyticsKey.BACKUP_DATA_SUCCESS)).isTrue()
         assertThat(fakeBackupMetadataRepo.updatedDate).isNull()
 
         val createdFiles =
@@ -220,6 +221,7 @@ class BackupDataWorkerTest {
         val result = worker.doWork()
 
         assertThat(result).isEqualTo(Result.success())
+        assertThat(analyticsHelper.hasLogged(AnalyticsKey.BACKUP_DATA_SUCCESS)).isTrue()
         assertThat(fakeBackupMetadataRepo.updatedDate).isNotNull()
 
         val createdFiles = tempDir.listFiles { f -> f.name.startsWith("SafeBoxBackup") }
@@ -282,6 +284,7 @@ class BackupDataWorkerTest {
         val result = worker.doWork()
 
         assertThat(result).isEqualTo(Result.success())
+        assertThat(analyticsHelper.hasLogged(AnalyticsKey.BACKUP_DATA_SUCCESS)).isTrue()
 
         val oldestFile = File(tempDir, "SafeBoxBackup20260101000000001.bak")
         assertThat(oldestFile.exists()).isFalse()
